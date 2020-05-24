79306a11a420bdb819df7d383ef815b591847074
heeking13 <513301743@qq.com>
2020-05-24 16:53:06 +0100

    good

---
+++ models/question.js
@@ -0,0 +1,14 @@
+const mongoose = require('../public/config').mongoose;
+const url = require('../public/config').url;
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://heeking13:111222hq@ds231517.mlab.com:31517/heroku_l1z5p2dp', { useNewUrlParser: true }).then(() => {
+    console.log('mongodb connect successfully');
+}, (err) => {
+    console.log(err);
+})
+
+
+//create the connection to the database
+const questionSchema = require('../scheams/question');
+module.exports = mongoose.model('question', questionSchema, 'question');
